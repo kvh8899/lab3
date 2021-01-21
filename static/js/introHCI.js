a274b76766c99982e65d04941baf432a05c5c98d
kvh8899 <kvhuang@ucsd.edu>
2021-01-21 10:07:35 -0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,18 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Loading...");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+		$(".jumbotron p").fadeOut();
 	});
-
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color","green");
+	var containingProject = $(this).closest(".project");
+	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
